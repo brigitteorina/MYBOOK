1fb9318844f7f095e773ac7bf05913f934967636
brigitteorina <brigitteorina@gmail.com>
2024-06-06 15:41:09 +0300

    Login page

--- login.php
+++ login.php
@@ -2,11 +2,89 @@
 
     <head>
         
-        <title>MyBook</title>
+        <title>MyBook  |Log in</title>
     </head>
 
-    <body>
+    <style>
+#bar{ 
+    height:100px; 
+    background-color: rgb(59,89,152); 
+    color: #d9dfeb;
+    padding: 4px;
+   
+ }
+
+ #signup_btn{
+    background-color:#42b72a; 
+    width:70px;
+    text-align: center;
+    padding: 4px;
+    border-radius: 4px;
+    float: right;
+ }
+
+ #login{
+    background-color: white; 
+    width: 800px; 
+    height: 200px;
+     margin:auto; 
+     margin-top:50px;
+     padding:10px;
+     text-align: center;
+     padding-top: 50px;
+     font-weight: bold;
+
+
+ }
+
+
+ #text{
+
+height: 40px;
+width: 300px;
+border-radius: 4px;
+border: solid 1px #ccc;
+padding: 4px;
+font-size: 14px;
+
+ }
+
+ #button{
+
+    width: 300px;
+    height: 40px;
+    border-radius: 4px;
+    font-weight: bold;
+    border: none;
+    background-color: rgb(59,89,152);
+    color:white;
+
+
+ }
+
+    </style>
+
+    <body style="font-family:tahoma; background-color:#e9ebee;" >
+
+<div id="bar">
+
+       <div style=" font-size : 40px;"> MyBook </div>
+        <div id="signup_btn" >SignUp </div>
+
+        </div>
+
+
+
+        <div id="login">
+            Log in to MyBook<br> <br>
+<input type="text" id="text" placeholder="Email"><br> <br> 
+<input type="password" id="text" placeholder="Password"><br> <br>
+<input type="Submit" id="button" value="Log in"><br> <br>
+
+
+
+        </div>
         
     </body> 
-    
+
 </html>
