996f33c3f43c2dac7b2d695c8bd77984723ce1ea
brigitteorina <brigitteorina@gmail.com>
2024-06-06 16:22:48 +0300

    signup page

---
+++ profile.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+    </head>
+
+
+    <style type="text/css">
+
+#blue_bar{
+
+}
+
+
+
+        </style>
+
+
+
+
+
+
+
+    <body>
+        <div id="blue_bar">
+
+
+        </div>
+        
+    </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,102 @@
+<html>
+
+    <head>
+        
+        <title>MyBook  |Sign up</title>
+    </head>
+
+    <style>
+#bar{ 
+    height:100px; 
+    background-color: rgb(59,89,152); 
+    color: #d9dfeb;
+    padding: 4px;
+   
+ }
+
+ #login_btn{
+    background-color:#42b72a; 
+    width:70px;
+    text-align: center;
+    padding: 4px;
+    border-radius: 4px;
+    float: right;
+ }
+
+ #signup{
+    background-color: white; 
+    width: 800px; 
+    height: 500px;
+     margin:auto; 
+     margin-top:50px;
+     padding:10px;
+     text-align: center;
+     padding-top: 50px;
+     font-weight: bold;
+
+
+ }
+
+
+ #text{
+
+height: 40px;
+width: 300px;
+border-radius: 4px;
+border: solid 1px #ccc;
+padding: 4px;
+font-size: 14px;
+
+ }
+
+ #button{
+
+    width: 300px;
+    height: 40px;
+    border-radius: 4px;
+    font-weight: bold;
+    border: none;
+    background-color: rgb(59,89,152);
+    color:white;
+
+
+ }
+
+    </style>
+
+    <body style="font-family:tahoma; background-color:#e9ebee;" >
+
+<div id="bar">
+
+       <div style=" font-size : 40px;"> MyBook </div>
+        <div id="login_btn" >Login </div>
+
+        </div>
+
+
+
+        <div id="signup">
+            Sign up to MyBook<br> <br>  
+           
+ <input type="text" id="text" placeholder="First Name"><br> <br> 
+ <input type="text" id="text" placeholder="Last Name"><br> <br> 
+ <span style="font-weight: normal;">Gender:</span><br>
+ <select id="text">
+
+ <option >Male</option>
+ <option >Female</option>
+ </select>
+ <br><br>
+
+<input type="text" id="text" placeholder="Email"><br> <br> 
+<input type="password" id="text" placeholder="Password"><br> <br>
+<input type="password" id="text" placeholder=" Confirm Password"><br> <br>
+<input type="Submit" id="button" value="SignUp"><br> <br>
+
+
+
+        </div>
+        
+    </body> 
+
+</html>
