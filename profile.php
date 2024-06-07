4a7caecafa29ff31e1e0c4be1e20756d767e06a5
brigitteorina <brigitteorina@gmail.com>
2024-06-07 09:48:15 +0300

    profile section

--- profile.php
+++ profile.php
@@ -31,6 +31,15 @@
 
 }
 
+#profile_pic{
+width: 150px;
+margin-top: -200px;
+border-radius: 50%;
+border: solid 2px white;
+
+
+}
+
 
 
         </style>
@@ -41,13 +50,33 @@
 
 
 
-    <body style="font-family: tahoma; ">
+    <body style="font-family: tahoma; background-color:#d0d8e4;">
+        <br>
+        <!--top bar -->
         <div id="blue_bar">
+
             <div style="width:800px; margin:auto; font-size:30px; ">
 MyBook &nbsp  &nbsp  <input type="text" id="search_box"  placeholder="search for people">
 <img src="images/selfie.jpg" style="width: 50px; float:right;">
-
             </div>
+
+        </div>
+        
+        <!--cover area-->
+        <div style="width:800px; margin:auto; background-color:black; min-height: 400px;">
+
+            <div style="background-color: white; text-align:center; color:#405d9b;">
+            <img src="images/mountain.jpg" style="width:100%;" >
+            <img id="profile_pic"  src="images/selfie.jpg" ><br>
+            Brigitte Orina
+<br>
+            Timeline  About Friends Photos Settings
+            </div>
+
+
+
+
+
 
 
         </div>
