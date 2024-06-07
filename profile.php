6ae22d80a3b9a81f45a614f368a692b2a18910a7
brigitteorina <brigitteorina@gmail.com>
2024-06-07 09:17:51 +0300

    search bar section

--- profile.php
+++ profile.php
@@ -2,13 +2,32 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title></title>
+        <title>Profile |MyBook</title>
     </head>
 
 
     <style type="text/css">
 
 #blue_bar{
+
+    height: 50px;
+    background-color: #405d9b;
+    color: d9dfeb;
+
+}
+
+#search_box{
+
+    width: 400px;
+    height: 20px;
+    border-radius: 5px;
+    border: none;
+    padding: 4px; 
+    font-size: 14px;
+    background-image: url(images/search.png);
+    background-repeat: no-repeat;
+    background-position: right;
+
 
 }
 
@@ -22,8 +41,13 @@
 
 
 
-    <body>
+    <body style="font-family: tahoma; ">
         <div id="blue_bar">
+            <div style="width:800px; margin:auto; font-size:30px; ">
+MyBook &nbsp  &nbsp  <input type="text" id="search_box"  placeholder="search for people">
+<img src="images/selfie.jpg" style="width: 50px; float:right;">
+
+            </div>
 
 
         </div>
